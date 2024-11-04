4786dd39194178adc087c0ab82a26e3aec46d7d7
williamdellaval <williamdellaval@gmail.com>
2024-11-03 21:19:03 -0300

    Alteracoes front end

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
-
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 </head>
 <body>
     <div class="container">
@@ -16,31 +17,45 @@
         <!-- Exemplo de interface com Bootstrap -->
         <div class="card mb-4">
             <div class="card-body">
-                <h5 class="card-title">Título do Card</h5>
-                <table id="myTable" class="display">
+                <h5 class="card-title">Catalogo de Filmes</h5>
+                <table id="myTable" class="display" style="width:100%">
                 <thead>
                     <tr>
-                        <th>Column 1</th>
-                        <th>Column 2</th>
+                        <th>Filme</th>
+                        <th>Sinopse</th>
+                        <th>Rating</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Row 1 Data 1</td>
-                        <td>Row 1 Data 2</td>
-                    </tr>
-                    <tr>
-                        <td>Row 2 Data 1</td>
-                        <td>Row 2 Data 2</td>
-                    </tr>
                 </tbody>
             </table>
             </div>
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-    <script src="js/script.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable({
+                paging: true,
+                searching: true,
+                ordering: true,
+                pageLength: 5,
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json'
+                },
+                ajax: {
+                    url: 'http://app-node:3000/movies', // URL da API
+                    dataSrc: 'movie' // Define a propriedade que contém os dados no JSON (ajuste conforme necessário)
+                },
+                columns: [
+                    { data: 'name' },       // Mapeia a coluna "Nome" com a propriedade "nome" no JSON
+                    { data: 'sinopsys' },      // Mapeia a coluna "Idade" com a propriedade "idade"
+                    { data: 'rating' },  // Mapeia a coluna "Profissão" com a propriedade "profissao"
+                ],
+            });
+        });
+    </script>
+
+    
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-$(document).ready( function () {
-    $('#myTable').DataTable();
-} );
+$(document).ready(function() {
+    $('#myTable').DataTable({
+        // Opções do DataTables
+        paging: true,         // Habilita paginação
+        searching: true,      // Habilita a barra de pesquisa
+        ordering: true,       // Habilita a ordenação nas colunas
+        pageLength: 5,        // Define o número de linhas por página
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/Portuguese-Brasil.json' // Traduz para Português
+        }
+    });
+});
